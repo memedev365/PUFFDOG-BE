e3bc89164284785c26d9d46b25fd781b5e221ad9
memedev365 <memedev365@outlook.com>
2025-04-25 19:17:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // CORS setup
 const corsOptions = {
-  origin: ['https://puffdog-fe.vercel.app'], // your frontend domain
+  origin: ['https://mint.puffdog.vip'], // your frontend domain
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // if your frontend needs cookies or auth
@@ -108,7 +108,7 @@ app.get('/api/events', (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://puffdog-fe.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://mint.puffdog.vip');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
